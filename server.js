bc9e8f4cf2c443084d8a9801ca3d92e845dfc1dd
Chalbert <chalbert.colon@gmail.com>
2020-05-19 22:35:17 -0400

    Add Port

--- server.js
+++ server.js
@@ -28,8 +28,8 @@ app.use(express.json())
 app.use(cors())
 
 // Listen ro port
-app.listen(3000, () => {
-    console.log('app is running on port 3000');
+app.listen(process.env.PORT || '3000', () => {
+    console.log(`app is running on port ${process.env.PORT}`);
 });
 
 // Routes
